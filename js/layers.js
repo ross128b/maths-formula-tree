908e480843f3935de668c92dd2861fad0d61b11d
ross128b <89728325+ross128b@users.noreply.github.com>
2021-09-27 18:02:14 +0800

    Add files via upload

---
+++ js/layers.js
@@ -0,0 +1,110 @@
+var tiu=0
+addLayer("u",{
+    name: "upgrade", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "U", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        //part1
+        unlocked: true,
+		points: new ExpantaNum(0),
+    }},
+    color: "lime",
+    resource: "upgrade", // Name of prestige currency
+    requires: new OmegaNum(10).pow(308), // Can be a function that takes requirement increases into account
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.05,
+    base: 10,
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new ExpantaNum(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new ExpantaNum(1)
+    },
+    row: 1,
+    upgrades: {
+        11:{
+            title:"buff or debuff",
+            cost: new OmegaNum(10),
+            description:"points gain x|2-log(points+1)|"+"<br>"+"+10^-8 and unlock 10 points hardcap",
+            effect(){
+                var eff=player.points
+                if(player.points.lt(99))return new OmegaNum(2).minus(eff.add(1).log().div(2.302585092994))
+                if(player.points.gte(99))return new OmegaNum(eff.add(1).log().div(2.302585092994)).minus(2)
+            },
+            effectDisplay() { return format(this.effect())+"x" }, // Add formatting to the effect
+            canAfford(){return player.points.gte(10)},
+            pay(){player.points = player.points.minus(10)},
+            unlocked() { return player[this.layer].unlocked }, // The upgrade is only visible when this is true
+        },
+        12: {
+            title:"too slow!",
+            cost:new OmegaNum(20),
+            description:"triple points gain",
+            canAfford(){return player.points.gte(20)},
+            pay(){player.points = player.points.minus(20)},
+            unlocked() { return (hasUpgrade(this.layer, 11))},
+        },
+        13: {
+            title:"still too slow",
+            cost:new OmegaNum(50),
+            description:"get a buff base on your time",
+            canAfford(){return player.points.gte(50)},
+            pay(){player.points = player.points.minus(50)},
+            effect(){
+                if(!hasUpgrade("u",14))return player.t.points.pow(0.25)
+                if(hasUpgrade("u",14))return player.t.points.pow(new OmegaNum(0.25).add(player.t.points.log().div(230.2585092994)))
+            },
+            effectDisplay() { return format(this.effect())+"x" }, // Add formatting to the effect
+            unlocked() { return ( hasUpgrade(this.layer, 12))},
+        },
+        14:{
+        	title:"u13 is not strong enough",
+        	cost:new OmegaNum(98.99),
+        	description:"u13 0.25->0.25+log(t)",
+        	canAfford(){return player.points.gte(98.99)},
+        	unlocked() { return (hasUpgrade(this.layer, 13))},
+        	pay(){player.points = player.points.minus(98.99)},
+
+
+        }
+    },
+    milestones: {
+        0: {
+            requirementDescription: "tips1",
+            done() {return player[this.layer].best.gte(0)}, // Used to determine when to give the milestone
+            effectDescription:"most of the upgrade in this layer need points instead of upgrade points to buy"
+        },
+    },
+})
+addLayer("t",{
+    name:"time",
+    symbol:"t",
+    position: 0,
+    startData() { return {
+        //part1
+        unlocked: false,
+        points: new ExpantaNum(0),
+    }},
+    color: "green",
+    resource: "time", // Name of prestige currency
+    requires: new OmegaNum(10), // Can be a function that takes requirement increases into account
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0,
+    base: 10,
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new ExpantaNum(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new ExpantaNum(1)
+    },
+    passiveGeneration(){
+        return 1
+    },
+    row: 1,
+})
---
+++ js/mod.js
@@ -0,0 +1,76 @@
+﻿let modInfo = {
+	name: "math formula tree",
+	id: "math formula tree",
+	author: "ross128b",
+	pointsName: "points",
+	discordName: "",
+	discordLink: "",
+	initialStartPoints: new ExpantaNum (0), // Used for hard resets and new players
+	
+	offlineLimit: 114514,  // In hours
+}
+
+// Set your version in num and name
+let VERSION = {
+	num: "0.0.1",
+	name: "",
+}
+
+let changelog = `<h1>Changelog:</h1><br>
+	<h3>v0.0</h3><br>
+		- Added things.<br>
+		- Added stuff.`
+
+let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+
+// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
+// (The ones here are examples, all official functions are already taken care of)
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+
+function getStartPoints(){
+    return new ExpantaNum(modInfo.initialStartPoints)
+}
+
+// Determines if it should show points/sec
+function canGenPoints(){
+	if(player.points.gte(10)&&!hasUpgrade("u",11))return false
+	return true
+}
+
+// Calculate points/sec!
+function getPointGen() {
+	if(!canGenPoints()) return new ExpantaNum(0)
+	let gain = new ExpantaNum(1)
+	if (hasUpgrade("u", 11)) gain = gain.times(upgradeEffect("u", 11).add(new OmegaNum(10).div(100000000)))
+	if (hasUpgrade("u", 12)) gain = gain.times(3)
+	if (hasUpgrade("u", 13)) gain = gain.times(upgradeEffect("u",13))
+
+	return gain
+}
+
+// You can add non-layer related variables that should to into "player" and be saved here, along with default values
+function addedPlayerData() { return {
+}}
+
+// Display extra things at the top of the page
+var displayThings = [
+]
+
+// Determines when the game "ends"
+function isEndgame() {
+	return false
+}
+
+
+
+// Less important things beyond this point!
+
+// You can change this if you have things that can be messed up by long tick lengths
+function maxTickLength() {
+	return(3600) // Default is 1 hour which is just arbitrarily large
+}
+
+// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
+// you can cap their current resources with this.
+function fixOldSave(oldVersion){
+}
